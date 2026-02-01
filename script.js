33846bc47e3e43d045fa91cfca76d20db566d681
nayaknirwan448-cell <nayaknirwan448@gmail.com>
2026-02-01 14:54:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const result = document.getElementById("result");
+const gif = document.getElementById("celebrateGif");
 
+// NO button runs away
 noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * (window.innerWidth - 100);
   const y = Math.random() * (window.innerHeight - 100);
   noBtn.style.left = x + "px";
-  no
+  noBtn.style.top = y + "px";
+});
+
+// YES button clicked
+yesBtn.addEventListener("click", () => {
+  result.style.display = "block";
+  gif.style.display = "block";
+});
